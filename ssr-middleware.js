ecb614905df09703ccad1e3b19e57afad5f11630
Steven Yee <steveny@Stevens-MacBook-Pro.local>
2017-08-03 21:20:16 -0700

    moved GA tracking tag

--- ssr-middleware.js
+++ ssr-middleware.js
@@ -38,15 +38,7 @@ const renderFullPage = (markup, data, page) => {
                  <link rel="shortcut icon" type="image/x-icon" href="/public/favicon.ico" />
                  <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cosmo/bootstrap.min.css" type="text/css" rel="stylesheet"/>
                  <link type="text/css" href="/public/widget.css" rel="stylesheet"/>
-            </head>
-
-            <body>
-                <div id="app">${markup}</div>
-                <script type="text/javascript">
-                window.__STATE__ = ${JSON.stringify(data)}
-                </script>
-                <script type="text/javascript" src=${jsLocation}></script>
-                <script>
+                 <script>
                     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -56,6 +48,14 @@ const renderFullPage = (markup, data, page) => {
                     ga('send', 'pageview');
 
                 </script>
+            </head>
+
+            <body>
+                <div id="app">${markup}</div>
+                <script type="text/javascript">
+                window.__STATE__ = ${JSON.stringify(data)}
+                </script>
+                <script type="text/javascript" src=${jsLocation}></script>
             </body>
         </html>
     `
