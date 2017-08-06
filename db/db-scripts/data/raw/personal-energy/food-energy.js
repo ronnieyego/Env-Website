042890b470011916fbe92cda002871cbfd12f3a3
Steven Yee <steveny@Stevens-MacBook-Pro.local>
2017-08-05 22:56:01 -0700

    added personal energy use data

---
+++ db/db-scripts/data/raw/personal-energy/food-energy.js
@@ -0,0 +1,68 @@
+// Hard to get this data
+// Source http://www.inference.org.uk/withouthotair/c13/page_77.shtml
+// Data shouls be viewed as approximations not fact
+
+// Changed some values
+/*
+    grain and vegetables were corn
+    dairy was cheese
+*/
+
+
+
+module.exports = [
+  {
+    "food": "grain",
+    "kwh-lb": 0.43,
+    "calories-lb": 390,
+    "use-type": "pound"
+  },
+  {
+    "food": "vegetables",
+    "kwh-lb": 0.43,
+    "calories-lb": 390,
+    "use-type": "pound"
+  },
+  {
+    "food": "milk",
+    "kwh-lb": 0.75,
+    "calories-lb": 291,
+    "use-type": "pound"
+  },
+  {
+    "food": "fruit",
+    "kwh-lb": 1.67,
+    "calories-lb": 1824,
+    "use-type": "pound"
+  },
+  {
+    "food": "eggs",
+    "kwh-lb": 4,
+    "calories-lb": 650,
+    "use-type": "pound"
+  },
+  {
+    "food": "chicken",
+    "kwh-lb": 4.4,
+    "calories-lb": 216,
+    "use-type": "pound"
+  },
+  {
+    "food": "dairy",
+    "kwh-lb": 6.75,
+    "calories-lb": 573,
+    "use-type": "pound"
+  },
+  {
+    "food": "pork",
+    "kwh-lb": 12.6,
+    "calories-lb": 480,
+    "use-type": "pound"
+  },
+  {
+    "food": "beef",
+    "kwh-lb": 31.5,
+    "calories-lb": 1176,
+    "use-type": "pound"
+  }
+]
