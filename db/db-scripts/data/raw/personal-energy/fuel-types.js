25fe3a7499b6afd67ada33d93624997224ff9275
Steven Yee <steveny@Stevens-MacBook-Pro.local>
2017-08-09 23:00:11 -0700

    added appliance and fuel data

---
+++ db/db-scripts/data/raw/personal-energy/fuel-types.js
@@ -0,0 +1,37 @@
+// this is energy and co2 from each fuel type
+// Source is the EIA
+// MPG not included but planes are around 90mpg/person
+
+module.exports = [
+  {
+    "fuel-type": "gasoline",
+    "kwhs": 34.4,
+    "co2": 19.64
+  },
+  {
+    "fuel-type": "diesel",
+    "kwhs": 37.95,
+    "co2": 22.38
+  },
+  {
+    "fuel-type": "liquid natural gas",
+    "kwhs": 21.75,
+    "co2": 15.65
+  },
+  {
+    "fuel-type": "bio-diesel",
+    "kwhs": 34.8,
+    "co2": 20.13
+  },
+  {
+    "fuel-type": "ethanol",
+    "kwhs": 22.27,
+    "co2": 12.72
+  },
+  {
+    "fuel-type": "jet-fuel",
+    "kwhs": 37.12,
+    "co2": 21.1
+  }
+]
+
