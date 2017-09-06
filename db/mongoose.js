bae268a50c915c3051fad85823006b29c282df59
Steven Yee <steveny@Stevens-MacBook-Pro.local>
2017-09-05 20:39:38 -0700

    changed from local to prod

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const local = 'mongodb://localhost:27017/EnvWebsite';
 const prod = 'mongodb://heroku_0fqtdb7x:sgvsobiubth5nmf1oujn6lr5jg@ds157571.mlab.com:57571/heroku_0fqtdb7x'
 
+const connection = process.env.page ? local : prod;
+
 mongoose.Promise = global.Promise;
-mongoose.connect(local);
+mongoose.connect(connection);
 
 module.export = {
     mongoose
