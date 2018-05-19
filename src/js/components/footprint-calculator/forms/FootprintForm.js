6bd13fe498cb07eea195c87e1b6c0fa54ee6e4c6
Steven Yee <steveny@Stevens-MacBook-Pro.local>
2018-05-19 00:08:23 -0700

    fixed typo

--- src/js/components/footprint-calculator/forms/FootprintForm.js
+++ src/js/components/footprint-calculator/forms/FootprintForm.js
@@ -116,7 +116,7 @@ export default class FootprintForm extends React.Component {
 		return (
       <div className="footprint-main">
         <div className="footprint-form-paragraphs">
-            <p>There is a ton of advice on how to lower your ecological footprint, "drive less, turn off your lights, dont' eat meat".  Sure, they're all good ideas, but have drastically different impacts.  One flaw I've found in all of these suggestions is that they take a qualitative approach to a very quantitative problem.</p>
+            <p>There is a ton of advice on how to lower your ecological footprint, "drive less, turn off your lights, don't eat meat".  Sure, they're all good ideas, but have drastically different impacts.  One flaw I've found in all of these suggestions is that they take a qualitative approach to a very quantitative problem.</p>
             <p>The calculator below attempts to give you reasonably accurate insights on your ecological footprint.  The goal is to give you the ability to reduce your footprint in a way that fits with your life.  The form takes about 5 minutes to fill out.</p>    
         </div>
         <div className="footprint-form">
