a58266aa8cca5fc8db6c887a8325183364ccd63c
Steven Yee <steveny@Stevens-MacBook-Pro.local>
2018-05-24 22:15:34 -0700

    added a footer to the costs/search page

--- src/js/components/costs/search/Search.js
+++ src/js/components/costs/search/Search.js
@@ -2,18 +2,16 @@ import React from "react";
 import PropTypes from 'prop-types';
 import AutoComplete from 'material-ui/AutoComplete';
 
+import CostsFooter from '../CostsFooter';
 import pages from '../pages-index';
 import ids from '../../../utils/ids/index';
 
 const data = Object.keys(pages);
 
 const onNewRequest = (chosenRequest, index) => {
-    console.log(`/costs/${chosenRequest}`);
     if(index !== -1) {
         window.location.href = `/costs/${chosenRequest}`
     }
-    console.log('chosenRequest', chosenRequest);
-    console.log('index', index);
 }
 
 export default class CostsSearch extends React.Component {
@@ -23,11 +21,14 @@ export default class CostsSearch extends React.Component {
 		return (
             <div className="costs">
                 <h3 className="costs-form-header">What's the CO<sub>2</sub> cost of things?</h3>
+                <p className="costs-form-sub-text">Use the search below to discover the CO<sub>2</sub> cost.</p>
                 <AutoComplete 
                     dataSource={data}
                     hintText="What are you looking for?"
                     onNewRequest={onNewRequest}
                 />
+
+                <CostsFooter />
             </div>
 		);
 	}
