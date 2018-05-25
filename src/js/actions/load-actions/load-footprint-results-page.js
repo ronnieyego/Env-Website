c90f2c8575d400b9102109206a6f46169afb2147
Steven Yee <steveny@Stevens-MacBook-Pro.local>
2018-05-24 22:54:31 -0700

    updated babel so I can use ASYNC AWAIT.  Awwww yeaaahhhh

--- src/js/actions/load-actions/load-footprint-results-page.js
+++ src/js/actions/load-actions/load-footprint-results-page.js
@@ -8,6 +8,7 @@ import { addMobileToStore } from '../../server/utils';
 
 // Database
 import { FormAnswers } from '../../../../db/models/form-answers';
+import { ObjectId } from 'mongodb';
 
 // Redux
 import { createStore } from 'redux';
@@ -18,40 +19,43 @@ import updateCostsReducer from '../../redux/update-reducer-by-page';
 
 // Pages
 
-export default (req, res) => {
-    const id = req.params.id;
-    FormAnswers.find({_id: id}).then(answers => {
-        const answer = answers[0];
-        if(!answer) {
-            res.send(404);
+export default async (req, res) => {
+    let id;
+    try {
+        id = ObjectId(req.params.id)
+    } catch(e) {
+        console.log('Invalid Id: ', req.params.id,  e);
+        res.status(500).send(`Invalid Id: ${req.params.id}`);
+    }
+    
+    const answers = await FormAnswers.find({_id: id});
+    if(!answers[0]) {
+        console.log(`Could not find Id: ${req.params.id}`);
+        res.status(404).send(`Could not find Id: ${req.params.id}`);
+    }
+    const answer = answers[0];
+    const storeData = {
+        footprintForm: {
+            questions: answer.questions,
+            getQuestionsError: false,
+            step: 1,
+            isSubmitReady: true,
+            questions: answer.formAnswers
+        },
+        footprintFormAnswers: {
+            ...baseState,
+            answerId: id,
+            formResults: answer.results
         }
-        const storeData = {
-            footprintForm: {
-                questions: answer.questions,
-                getQuestionsError: false,
-                step: 1,
-                isSubmitReady: true,
-                questions: answer.formAnswers
-            },
-            footprintFormAnswers: {
-                ...baseState,
-                answerId: id,
-                formResults: answer.results
-            }
-        };
-        const appendedStoreData = addMobileToStore(req, storeData);
-        appendedStoreData.page = 'footprintresult';
-        const store = createStore(reducers, appendedStoreData);
-        const appMarkup = ReactDOM.renderToString(
-            <Provider store={store}>
-                <MuiThemeProvider>
-                    <div />
-                </MuiThemeProvider>
-            </Provider>);
-        res.status(200).send(renderFullPage(appMarkup, appendedStoreData, 'static-pages'));
-    })
-    .catch(e => {
-        console.log('error loading footprint by id ', e);
-        res.send(500);
-    });
+    };
+    const appendedStoreData = addMobileToStore(req, storeData);
+    appendedStoreData.page = 'footprintresult';
+    const store = createStore(reducers, appendedStoreData);
+    const appMarkup = ReactDOM.renderToString(
+        <Provider store={store}>
+            <MuiThemeProvider>
+                <div />
+            </MuiThemeProvider>
+        </Provider>);
+    res.status(200).send(renderFullPage(appMarkup, appendedStoreData, 'static-pages'));
 };
