c5beea34ecc67ae1c0d39fe9c563a70a5586392b
Steven Yee <steveny@Stevens-MacBook-Pro.local>
2018-06-20 21:17:15 -0700

    deploying to Heroku

---
+++ src/js/components/v2-form/data/food.js
@@ -0,0 +1,22 @@
+import ids from '../../../utils/ids/index';
+
+const bacon = 'lol'
+
+const foodAnswerServings = {
+
+    [ids.grainFrequency]: {
+        'Never': 0,
+        'Rarely': .25,
+        'A little bit': 1,
+        'A few servings per day': 3,
+        'As much as possible': 7
+    },
+
+    [ids.fruitFrequency]: {
+        'Never': 0,
+        'Rarely': 1,
+        'A little bit': 2,
+        'A few servings per day': 3,
+        'As much as possible': 4
+    }
+}
