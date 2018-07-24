a0cb885c8e4241ac051b3f7ce0c54b1bb204d6d7
Steven Y <stevenyee99@gmail.com>
2018-07-23 22:30:33 -0700

    improvements to the desktop bar chart with multiple values

--- src/js/components/bar-chart/BarChartDesktop.js
+++ src/js/components/bar-chart/BarChartDesktop.js
@@ -4,6 +4,29 @@ import { array, bool, string } from 'prop-types';
 import { BarChart, Bar, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import { roundData } from './utils';
 
+const INDEX_LIMIT_FOR_LOWER_RENDER = 5;
+
+const renderOffsetTicks = (props) => {
+    const { x, y, payload } = props;
+    const { index } = payload;
+    const offset = index % 2 === 0 ? 0 : 20;
+
+    return (
+        <text x={x} y={y + offset} textAnchor="middle" dominantBaseline="hanging">	
+            {payload.value}
+        </text>
+  );
+};
+
+const renderDefaultTicks = (props) => {
+    const { x, y, payload } = props;
+    return (
+        <text x={x} y={y} textAnchor="middle" dominantBaseline="hanging">	
+            {payload.value}
+        </text>
+  );
+};
+
 export default class BarCharDesktop extends React.Component {
 
     static propTypes = {
@@ -14,7 +37,7 @@ export default class BarCharDesktop extends React.Component {
         units: string, //units on left side
         title: string
     }
-        
+
     getDomainMax(barGraphData) {
         let max = 0;
         barGraphData.forEach(ob => {
@@ -29,9 +52,19 @@ export default class BarCharDesktop extends React.Component {
         return max;
     }
 
+    getTicks(barGraphData) {
+        return barGraphData.map((data, index) => {
+            const tickSize = index % 2 === 0 ? 25 : 6;
+            return { ...data, tickSize }
+        });
+    }
+
     render() {
-    
         const barGraphData = roundData(this.props.graphData);
+        const lowerRender = barGraphData.length > INDEX_LIMIT_FOR_LOWER_RENDER;
+        const labelYOffset = lowerRender ? '-20px' : '5px';
+        const renderTicks = lowerRender ? renderOffsetTicks : renderDefaultTicks;
+        const tickSize = lowerRender ? 10 : 6
         const defaultMax = this.props.defaultMax || 0;
         const domainMax = this.getDomainMax(barGraphData);
         const max = domainMax > defaultMax ? domainMax : defaultMax;
@@ -46,11 +79,11 @@ export default class BarCharDesktop extends React.Component {
                 <BarChart width={600} height={300} data={barGraphData}
                         margin={{top: 5, right: 30, left: 20, bottom: 5}}
                         className="bar-chart">
-                    <XAxis dataKey="name"/>
+                    <XAxis dataKey="name" interval={0} tickSize={tickSize} tick={renderTicks} />
                     <YAxis type="number" domain={[0, max]} label={{ value: units, angle: -90, position: 'left' }} />
                     <CartesianGrid strokeDasharray="3 3"/>
                     <Tooltip/>
-                    <Legend wrapperStyle={{marginLeft: '0px', marginTop: '30px'}} />
+                    <Legend wrapperStyle={{marginLeft: '0px', bottom: labelYOffset}} />
                     <ReferenceLine y={0} stroke='#000'/>
                     <Bar dataKey={dataKey} fill="#8884d8" />
                     {this.props.compare && <Bar dataKey={dataKeyCompare} fill="#82ca9d" />}
